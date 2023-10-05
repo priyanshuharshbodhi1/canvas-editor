14396194fc861f887d7bc93772f4e5cbf2567410
priyanshu <priyanshuqpwp@gmail.com>
2023-10-05 22:39:51 +0530

    created top and bottom bar in Create Porject

--- App.js
+++ App.js
@@ -42,6 +42,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-evenly"
   },
 });
--- screens/CreateProject.jsx
+++ screens/CreateProject.jsx
@@ -1,10 +1,89 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function CreateProject() {
+export default function CreateProject({ navigation }) {
+  const handleBack = () => {
+    navigation.goBack();
+  };
+
+  
+  const handleUndo = () => {
+    
+  };
+
+  
+  const handleRedo = () => {
+    
+  };
+
+  
+  const handleSave = () => {
+    
+  };
+
+  const handleText = () => {
+    
+  };
+
   return (
-    <View>
-      <Text>Create New Project Screen</Text>
+    <View style={styles.container}>
+
+      <View style={styles.topBar}>
+        <TouchableOpacity onPress={handleBack}>
+          <Text style={styles.button}>Back</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleUndo}>
+          <Icon name="undo" size={35} color="grey" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleRedo}>
+          <Icon name="redo" size={35} color="grey" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleSave}>
+          <Text style={styles.button}>Save</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.space} />
+
+      <TouchableOpacity style={styles.bottomBar} onPress={handleText}>
+        <Text style={styles.bottomButton}>TEXT</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+      },
+  topBar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#eee",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  button: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "grey",
+    marginLeft: 10,
+  },
+  space: {
+    flex: 1,
+  },
+  bottomBar: {
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+  },
+  bottomButton: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
